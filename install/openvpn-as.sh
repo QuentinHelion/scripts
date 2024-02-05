7bbe05c791435235abd3db997aa9fa9a61b19f79
QuentinHelion <helion.quentin@gmail.com>
2024-02-05 18:31:25 +0100

    update openvpn-as install script

--- install/openvpn-as.sh
+++ install/openvpn-as.sh
@@ -10,7 +10,45 @@ fi
 # no display output
 # > /dev/null 2>&1
 
+
+# ==== Installation part ====
 apt-get update > /dev/null 2>&1
 apt-get upgrade -y > /dev/null 2>&1
 
-apt-get install openvpn-as -y > /dev/null 2>&1
+apt update && apt -y install ca-certificates wget net-tools gnupg > /dev/null 2>&1
+wget https://as-repository.openvpn.net/as-repo-public.asc -qO /etc/apt/trusted.gpg.d/as-repository.asc > /dev/null 2>&1
+
+echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/as-repository.asc] http://as-repository.openvpn.net/as/debian bookworm main">/etc/apt/sources.list.d/openvpn-as-repo.list 
+
+apt update && apt -y install openvpn-as > /dev/null 2>&1
+
+cat /usr/local/openvpn_as/init.log
+
+
+# ==== Configuration part ====
+cd /usr/local/openvpn_as/scripts/
+
+DEFAULT_ADMIN = "Op3n4dmin"
+DEFAULT_PASSW = "4dmin2024"
+DEFAULT_GROUP = "default_group"
+
+create_user "$DEFAULT_ADMIN"
+
+sh sacli --user  --key "type" --value "user_connect" UserPropPut
+sh sacli --user "$DEFAULT_ADMIN" --new_pass "$DEFAULT_PASSW" SetLocalPassword
+
+
+./sacli --user "$DEFAULT_GROUP" --key "type" --value "group" UserPropPut
+./sacli --user "$DEFAULT_GROUP" --key "group_declare" --value "true" UserPropPut
+
+./sacli --user "$DEFAULT_ADMIN" --key "conn_group" --value "$DEFAULT_GROUP" UserPropPut
+
+
+./sacli start
+
+# ==== Functions ====
+create_user() {
+    adduser --gecos "$1" "$2"
+
+    echo "$1:$password" | chpasswd
+}
